3423f190fbb717d888b5325dbdbf4c7e13bbca9b
Mike Carouchi <mikecarouchi@gmail.com>
2022-11-27 02:31:15 -0700

    complete backend with full user login, logout, user posts, and full blog functions

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -8,7 +8,7 @@ export const register = (req, res) => {
 	const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 
 	db.query(q, [req.body.email, req.body.username], (err, data) => {
-		if (err) return res.json(err);
+		if (err) return res.status(500).json(err);
 		if (data.length) return res.status(409).json("User already exists!");
 
 		// Hash password / create user
@@ -19,7 +19,7 @@ export const register = (req, res) => {
 		const values = [req.body.username, req.body.email, hash];
 
 		db.query(q, [values], (err, data) => {
-			if (err) return res.json(err);
+			if (err) return res.status(500).json(err);
 			return res.status(200).json("User Created!");
 		});
 	});
@@ -31,8 +31,8 @@ export const login = (req, res) => {
 	const q = "SELECT * FROM users WHERE username = ?";
 
 	db.query(q, [req.body.username], (err, data) => {
-		if (err) return res.json(err);
-		if (data.length === 0) return response.status(404).json("User not found");
+		if (err) return res.status(500).json(err);
+		if (data.length === 0) return res.status(404).json("User not found");
 
 		// password check
 		const isPasswordCorrect = bcrypt.compareSync(
--- api/index.js
+++ api/index.js
@@ -3,11 +3,28 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import multer from "multer"; // allows to route uploads to a folder / set destination
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "../front/public/upload");
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + file.originalname);
+	},
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+	const file = req.file;
+	res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
